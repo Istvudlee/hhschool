5da3698e8c72e9dbe82d299c6be1b23407fb9e8c
Istvudlee <istvudlee@mail.ru>
2019-11-21 15:44:41 +0300

    Create project

---
+++ school/school/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  school
+//
+//  Created by Анатолий Оруджев on 21.11.2019.
+//  Copyright © 2019 Head and Hands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
